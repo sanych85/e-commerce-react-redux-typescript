67b314b27a069b78e873cc6ee84d68656010186f
Aleks Shatokhin <aleks_shatoh@mail.ru>
2021-11-04 01:40:34 +0300

    add filters Part 3

--- src/store/reducers/filterReducer/filterReducer.ts
+++ src/store/reducers/filterReducer/filterReducer.ts
@@ -8,64 +8,98 @@ const ititialState: IFilter = {
   filteredProducts: [],
   filters: {
     text: '',
-    category: {
-
-    },
+    category: {},
     minPrice: 0,
     maxPrice: 200,
-   
   },
- 
 };
 
 export const filterReducer = (state = ititialState, action: FilterAction) => {
-    
   switch (action.type) {
     case FilterActionsEnum.SUCCESS_LOAD_FILTER_ITEMS: {
       return {
-        ...state, filteredProducts: action.payload.data, filters: {
-          ...state.filters, category:action.payload.category
-        }
-
-      }
+        ...state,
+        filteredProducts: action.payload.data,
+        filters: {
+          ...state.filters,
+          category: action.payload.category,
+        },
+      };
     }
     case FilterActionsEnum.FILTER_ITEMS: {
-      console.log('Action paylaod', action.payload.value)
-        let updatedFilters = {...state.filters}; 
+      console.log('Action paylaod', action.payload.value);
+      let updatedFilters = { ...state.filters };
       if (action.payload.variant === 'text') {
-         updatedFilters = {
-            ...state.filters,
-            text:action.payload.value 
-          ,
-        };
-        console.log(updatedFilters, "updated filters")
-
-      } else if (action.payload.variant === 'category') {
-        console.log("value", action.payload.value)
-        console.log("type", state.filters.category[action.payload.value])
-        console.log("state.filters.category", state.filters.category)
         updatedFilters = {
-            
-                ...state.filters, category: {
-                    ...state.filters.category, [action.payload.value]:!state.filters.category[action.payload.value] 
-                }
-            
-        }
-        
+          ...state.filters,
+          text: action.payload.value,
+        };
+        console.log(updatedFilters, 'updated filters');
+      } else if (action.payload.variant === 'category') {
+        updatedFilters = {
+          ...state.filters,
+          category: {
+            ...state.filters.category,
+            [action.payload.value]:
+              !state.filters.category[action.payload.value],
+          },
+        };
       }
-      console.log("updated filters", updatedFilters)
-      
-      return  {
-        ...state, filters: updatedFilters  
-          , filteredProducts: action.payload.products.filter((product:IProduct):IProduct[] | boolean  => {
-        
-            return product.title.toLowerCase().startsWith(action.payload.value)
-        }),  
-      
-    }
 
    
+      const updatedState = () => {
+        const filteredCategories:string[] = Object.entries(updatedFilters.category).filter(
+          (item) => item[1] 
+        ).map(item=>item[0]);
+     
+      
+        let filteredProducts = action.payload.products.filter(
+          (product: IProduct): IProduct | boolean   => {
+            console.log(action.payload.value)
+            if(updatedFilters.text!=="")  {
+              
+              return product.title.toLowerCase().startsWith(action.payload.value);
+            }
+            else {
+            
+              return product
+            }
+            
+          }
+        );
+        console.log(filteredProducts, "filteredProducts after text")
+        let filteredArray:any = []
+          if(filteredCategories.length>0) {
+            console.log("in if")
+            
+           filteredCategories.forEach((category:string)=> {
+              const filtersByCategoryItems = filteredProducts.filter(item=>item.category===category)
+              console.log(filtersByCategoryItems, "filteredProducts")
+              filteredArray = [...filteredArray, ...filtersByCategoryItems]
+             console.log(filteredArray) 
+             
+           })
+           
+          }else {
+            console.log("in else")
+            filteredArray  = filteredProducts 
+          }
+   
+          console.log(filteredArray, "final filter") 
+        return filteredArray
+        
+      };
+     
+      // console.log(updatedState(), "glbal update state")
+      
+ 
+      return {
+        ...state,
+        filters: updatedFilters,
+        filteredProducts: updatedState()
+      };
+    }
+    default:
+      return state;
   }
-  default: return state;
 };
-}
